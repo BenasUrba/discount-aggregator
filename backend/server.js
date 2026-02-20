01f4e660f657558ca11e69c2c88e979cdb0264f1
BenasUrba <benasurba@gmail.com>
2026-02-20 17:33:17 +0200

    fix: update pool import for controller and PORT in server

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const productsRouter = require('./routes/products');
 app.use(express.json());
 app.use('/api/products', productsRouter);
 
-const PORT = process.env.DB_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
