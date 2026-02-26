d78b46f8461e9dd47b884582204316dfbda85689
BenasUrba <benasurba@gmail.com>
2026-02-26 18:31:17 +0200

    chore: add hero component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,9 @@
-import './index.css';
-import Home from "./pages/home";
 import Hero from "./components/Hero";
-const API_URL = import.meta.env.VITE_API_URL;
-
-const stores = ["All Stores", "Lidl", "IKI", "Maxima", "Rimi"]
 
 function App() {
-
-  const handleSelectStore = async (store) => {
-    console.log(`Selected Store: ${store}`);
-    const response = await fetch(`${API_URL}/api/products?store=${store}`); 
-    
-    return response.json()
-  }
-
   return (
     <div className="min-h-screen bg-gray-100">
-      <Hero
-        stores={stores}
-        onSelectStore={handleSelectStore}
-      />
+      <Hero/>
     </div>
   )
 }
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,5 +1,4 @@
 export default function Hero() {
-
     return (
         <header className="max-w-6xl mx-auto px-6 py-16 text-center">
             <h1 className="text-5xl font-extrabold text-gray-900 mb-4">
