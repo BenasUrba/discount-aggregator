507054a92665411b17082ba83eead302aafaff19
BenasUrba <benasurba@gmail.com>
2026-03-04 10:57:35 +0200

    feat: added product skeleton cards whilst loading products

--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -2,11 +2,15 @@ import ProductCard from "./ProductCard";
 
 export default function ProductList({ products }) {
     if (!products || products.length === 0) {
-        return <p>No Products Found.</p>
+        return (
+            <div className="mt-12 text-center text-gray-500">
+                No products found.
+            </div>
+        )
     }
 
     return (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-8 px-4 sm:px-6 lg:px-24 justify-items-center">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-8 px-4 sm:px-6 lg:px-24">
             {products.map((product) => (
                 <ProductCard 
                     key={product.id}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getProducts } from "../services/api";
 import Hero from "../components/Hero";
 import ProductList from "../components/ProductList";
+import ProductCardSkeleton from "../components/ProductCardSkeleton";
 
 const stores = ["All Stores", "Lidl", "IKI", "Maxima", "Rimi"];
 
@@ -37,7 +38,11 @@ export default function Home() {
                 onSelectStore={setSelectedStore}
             />
 
-            {isLoading && <div className="mt-6 p-6">Loading Products...</div>}
+            {isLoading && <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-8 px-4 sm:px-6 lg:px-24">
+                    {Array.from({ length: 10 }).map((_, i) => (
+                        <ProductCardSkeleton key={i} />
+                    ))}
+                </div>}
             {error && <div className="mt-6 p-6 text-red-600">{error}</div>}
 
             {!isLoading && !error && (
