0b3826e6815d9b4e0d548f49d7cd29e23236c5c9
BenasUrba <benasurba@gmail.com>
2026-03-10 10:57:20 +0200

    feat: added loyalty card icons for products which required loyalty cards

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -10,12 +10,26 @@ export default function ProductCard({ product }) {
             `Iki ${formatDate(product.valid_until)}`:
             null;
 
+    const loyaltyBadge = {
+        Lidl: { src: "/lidlPlus-icon.png", className: "h-8 w-8"},
+        IKI: { src: "/ikiLoyalty-icon.png", className: "h-6 w-8"},
+        Maxima: { src: "/maximaLoyalty-icon.png", className: "h-6 w-8"},
+        Rimi: { src: "/rimiLoyalty-icon.png", className: "h-7 w-8"}
+    }
+
     return(
         <div className="bg-white border rounded-lg p-4 shadow-sm w-full flex flex-col h-full">
             <div className="relative h-40 w-full flex justify-center items-start">
                 <img src={product.image} alt={product.title} className="max-h-full object-contain"/>
                 {product.discount_info && (
                     <p className="absolute top-1 right-1 bg-red-600 px-2 py-1 rounded text-white text-sm rotate-6 shadow-md">{product.discount_info}</p>
+                )}
+                {product.loyalty_required && loyaltyBadge[product.store] && (
+                    <img
+                        src={loyaltyBadge[product.store].src}
+                        className={`absolute bottom-2 right-2 rounded ${loyaltyBadge[product.store].className}`}
+                        alt={`${product.store} loyalty badge`}
+                    />
                 )}
             </div>
             {product.product_brand && (
