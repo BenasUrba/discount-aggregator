98ec74e1a1cafcdd6212fa6a390a41dfc4c24358
BenasUrba <benasurba@gmail.com>
2026-03-31 12:30:38 +0300

    feat: added get route for top product discounts

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { getAllProducts } = require('../controllers/productsController');
+const { getAllProducts, getTopDiscounts } = require('../controllers/productsController');
 
 router.get('/', getAllProducts);
+
+router.get('/top', getTopDiscounts);
 
 module.exports = router;
