7b81bf33a03950140e52b3ae746b2dc1961f7ce9
BenasUrba <benasurba@gmail.com>
2026-04-02 15:36:19 +0300

    feat: added logic to remove carousel on page change or if user inputs text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -83,10 +83,10 @@ export default function Home() {
                 setSearch={setSearch}
             />
 
-            <Carousel
+            {currentPage === 1 && search.trim() === "" && carouselError === null && carouselLoading === false && (<Carousel
                 key={selectedStore}
                 products={topDiscounts}
-            />
+            />)}
 
             {isLoading && <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 mt-8 px-4 sm:px-6 lg:px-24">
                     {Array.from({ length: 10 }).map((_, i) => (
